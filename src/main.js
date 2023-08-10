314d729d0d6f712f95bdc4fc2fb0034ac06c868b
Artemka62 <123449625+Artemka62@users.noreply.github.com>
2023-08-10 17:15:43 +0400

    Добавил презентер отрисовки header

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
-import NewTaskFilterTimeView from './view/new-task-filter-time-view.js';
-import NewTaskHeaderTravelView from './view/new-tack-header-cost-trip-view.js';
+import HeaderMainPresenter from './presenter/header-presenter.js';
+
+
 import NewTaskFormSortFilterView from './view/new-task-trip-sort-view.js';
 import NewTaskFormEventView from './view/new-task-trip-events-list-view.js';
 import NewTaskFormEditEventView from './view/new-task-trip-events-item.js';
@@ -8,13 +9,15 @@ import {render} from './render.js';
 
 
 const siteBodyElement = document.querySelector('.page-header');
+const siteTripInfo = siteBodyElement.querySelector('.trip-main');
 const placeFilerHeader = siteBodyElement.querySelector('.trip-controls__filters');
+const headerMainPresenter = new HeaderMainPresenter({tripInfoContainer: siteTripInfo, tripFilterContainer: placeFilerHeader});
+
+
 const siteMainElement = document.querySelector('.page-main');
 const placeEventsMain = siteMainElement.querySelector('.trip-events');
 
 
-render(new NewTaskHeaderTravelView(), placeFilerHeader);
-render(new NewTaskFilterTimeView(), placeFilerHeader);
 render(new NewTaskFormSortFilterView(), placeEventsMain);
 render(new NewTaskFormEventView(), placeEventsMain);
 
@@ -26,4 +29,4 @@ for(let i = 0; i <= 3; i++){
   render(new NewTaskListRoadView(), placeFormEventsMain);
 }
 
-
+headerMainPresenter.init();
