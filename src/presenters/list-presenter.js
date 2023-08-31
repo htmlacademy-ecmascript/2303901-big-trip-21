2639f7a224efd698dee427c98a14b49c859bec78
Artemka62 <123449625+Artemka62@users.noreply.github.com>
2023-08-31 16:53:20 +0400

    Добавили в ListPresenter метод создания  PointModel из atemState

--- src/presenters/list-presenter.js
+++ src/presenters/list-presenter.js
@@ -50,7 +50,7 @@ class ListPresenter extends Presenter {
 
         offers: offers.map((offer) => ({
           ...offer,
-          isSelected: point.offersIds?.includes(offer.id)
+          isSelected: point.offerIds?.includes(offer.id)
         })),
 
         isFavorite: point.isFavorite,
@@ -59,6 +59,26 @@ class ListPresenter extends Presenter {
     });
 
     this.view.setState({items});
+  }
+
+  /**
+   * @param {import('../views/list-view').ItemState} state
+   * @returns {import('../models/point-model).default} state
+   */
+  createPoint(state) {
+    const point = this.model.createPoint();
+    Object.assign(point, {
+      id: state.id,
+      type: state.types.find((type) => type.isSelected).value,
+      destinationId: state.destinations.find((destination) => destination.isSelected)?.id,
+      dateFrom: state.dateFrom,
+      dateTo: state.dateTo,
+      basePrice: state.basePrice,
+      offerIds: state.offers.filter((offer) => offer.isSelected).map((offer) => offer.id),
+      isFavorite: state.isFavorite
+    });
+
+    return point;
   }
 
   /**
@@ -91,7 +111,7 @@ class ListPresenter extends Presenter {
     const card = event.target;
     card.state.isFavorite = !card.state.isFavorite;
     //TODO обновить модель
-    console.log(this.model.createPoint());
+    console.log(this.createPoint(card.state));
     card.render();
   }
 }
